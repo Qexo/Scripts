00734c67706c52244814b890d82863447634d900
Abudu <abudulin@foxmail.com>
2026-01-09 21:34:03 +0800

    fix: 用户获取错误

--- scripts/delete-account.py
+++ scripts/delete-account.py
@@ -13,6 +13,7 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
+user = User.objects.filter(username=username).first()
 if user is not None:
     #if User.objects.filter(is_staff=True).count() or not user.is_staff:  #Failes
     if User.objects.filter(is_staff__in=[True]).count() or not user.is_staff:  # Works 
